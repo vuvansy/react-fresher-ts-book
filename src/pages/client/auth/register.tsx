1de38eb499bfe6479c64fb21315cbf4b84a48dc8
vuvansy <vuvansy3001@gmail.com>
2024-12-04 16:23:32 +0700

    Design Register Form

--- src/pages/client/auth/register.tsx
+++ src/pages/client/auth/register.tsx
@@ -1,8 +1,89 @@
+import { Button, Divider, Form, Input } from 'antd';
+import type { FormProps } from 'antd';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './register.scss';
+
+type FieldType = {
+    fullName: string;
+    email: string;
+    password: string;
+    phone: string;
+};
 
 const RegisterPage = () => {
+
+    const [isSubmit, setIsSubmit] = useState(false); //Trạng thái loading ở nút đăng ký Atd
+
+    const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+        console.log(values)
+    };
+
     return (
-        <div>
-            RegisterPage
+        <div className="register-page">
+            <main className="main">
+                <div className="container">
+                    <section className="wrapper">
+                        <div className="heading">
+                            <h2 className="text text-large">Đăng Ký Tài Khoản</h2>
+                            <Divider />
+                        </div>
+                        <Form
+                            name="form-register"
+                            onFinish={onFinish} //Hàm nhấn submit form
+                            autoComplete="off"
+                        >
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Họ tên"
+                                name="fullName"
+                                rules={[{ required: true, message: 'Họ tên không được để trống!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Email"
+                                name="email"
+                                rules={[
+                                    { required: true, message: 'Email không được để trống!' },
+                                    { type: "email", message: "Email không đúng định dạng!" }
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[{ required: true, message: 'Mật khẩu không được để trống!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Số điện thoại"
+                                name="phone"
+                                rules={[{ required: true, message: 'Số điện thoại không được để trống!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" loading={isSubmit}>
+                                    Đăng ký
+                                </Button>
+                            </Form.Item>
+                            <Divider>Or</Divider>
+                            <p className="text text-normal" style={{ textAlign: "center" }}>
+                                Đã có tài khoản ?
+                                <span>
+                                    <Link to='/login' > Đăng Nhập </Link>
+                                </span>
+                            </p>
+                        </Form>
+                    </section>
+                </div>
+            </main>
         </div>
     )
 }
