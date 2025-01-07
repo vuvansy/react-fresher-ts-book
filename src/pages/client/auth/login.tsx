e6825d0bc812f2ddb3f9f85d218963a58a8d2571
vuvansy <vuvansy3001@gmail.com>
2025-01-07 11:56:11 +0700

    Chức năng Login

--- src/pages/client/auth/login.tsx
+++ src/pages/client/auth/login.tsx
@@ -1,9 +1,92 @@
+import { App, Button, Divider, Form, Input } from 'antd';
+import { Link, useNavigate } from 'react-router-dom';
+import './login.scss';
+import { useState } from 'react';
+import type { FormProps } from 'antd';
+import { loginAPI } from '@/services/api';
+
+type FieldType = {
+    username: string;
+    password: string;
+};
 
 
 const LoginPage = () => {
+
+    const navigate = useNavigate();
+    const [isSubmit, setIsSubmit] = useState(false);
+    const { message, notification } = App.useApp();
+
+    const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+        // console.log(values);
+        const { username, password } = values;
+        setIsSubmit(true);
+        const res = await loginAPI(username, password);
+        setIsSubmit(false);
+        if (res?.data) {
+            localStorage.setItem('access_token', res.data.access_token);
+            message.success("Đăng nhập tài khoản thành công!");
+            navigate("/")
+        } else {
+            notification.error({
+                message: "Có lỗi xảy ra",
+                description:
+                    res.message && Array.isArray(res.message) ? res.message[0] : res.message,
+                duration: 5
+            })
+        }
+    }
+
     return (
-        <div>
-            LoginPage
+        <div className="login-page">
+            <main className="main">
+                <div className="container">
+                    <section className="wrapper">
+                        <div className="heading">
+                            <h2 className="text text-large">Đăng Nhập</h2>
+                            <Divider />
+                        </div>
+                        <Form
+                            name="login-form"
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Email"
+                                name="username"
+                                rules={[
+                                    { required: true, message: 'Email không được để trống!' },
+                                    { type: "email", message: "Email không đúng định dạng!" }
+                                ]}
+                            >
+                                <Input />
+                            </Form.Item>
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[{ required: true, message: 'Mật khẩu không được để trống!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" loading={isSubmit}>
+                                    Đăng nhập
+                                </Button>
+                            </Form.Item>
+                            <Divider>Or</Divider>
+                            <p className="text text-normal" style={{ textAlign: "center" }}>
+                                Chưa có tài khoản ?
+                                <span>
+                                    <Link to='/register' > Đăng Ký </Link>
+                                </span>
+                            </p>
+                            <br />
+                        </Form>
+                    </section>
+                </div>
+            </main>
         </div>
     )
 }
--- src/pages/client/auth/register.tsx
+++ src/pages/client/auth/register.tsx
@@ -1,4 +1,4 @@
-import { App, Button, Divider, Form, Input, message } from 'antd';
+import { App, Button, Divider, Form, Input } from 'antd';
 import type { FormProps } from 'antd';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
